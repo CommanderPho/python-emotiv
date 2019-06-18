8e52a29e5d96f09227215e83bb0d5f1f1fa7ac56
Pho Hale <phohale@gmail.com>
2019-06-18 06:57:21 -0400

    Updated it to send a second stream of data containing the battery level and the signal qualities

--- examples/pho-server-record.py
+++ examples/pho-server-record.py
@@ -19,6 +19,7 @@ import json
 UDP_IP_ADDRESS = "10.0.0.90"
 UDP_PORT_NO = 6789
 Message = "Hello, Server"
+PacketSendDuration = 9 #Specifies how long between signal quality packets and saving to .mat file. In seconds.
 
 try:
 	from emotiv import epoc, utils
@@ -35,7 +36,7 @@ def callback_single_sample_complete(outlet, sampleData):
 
 def headsetRead(headset, single_sample_read_callback):
 	#idx, data = headset.acquire_data_fast(9)
-	data = headset.acquire_data(9, sample_callback=single_sample_read_callback)
+	data = headset.acquire_data(PacketSendDuration, sample_callback=single_sample_read_callback)
 	print "Battery: %d %%" % headset.battery
 	print "Contact qualities"
 	print headset.quality
@@ -46,15 +47,23 @@ def input_thread(a_list):
 	raw_input()
 	a_list.append(True)
 
-def dataAcquisitionLoop(headset, outlet, clientSock):
+def dataAcquisitionLoop(headset, outlets, clientSock):
 	a_list = []
 	thread.start_new_thread(input_thread, (a_list,))
 	# Build the callback function as a lambda function
-	callback_single_single_sample_complete_with_outlet = lambda d: callback_single_sample_complete(outlet, d)
+	callback_single_single_sample_complete_with_outlet = lambda d: callback_single_sample_complete(outlets['data'], d)
 	while not a_list:
 		data, metadata = headsetRead(headset, callback_single_single_sample_complete_with_outlet)
 		# Push the complete metadata on write
-		#
+		metadataOutputVector = []
+		# print("battery type:", type(float(metadata["battery"])))
+		metadataOutputVector.append(float(metadata["battery"]))
+		# print("quality type:", type(metadata["quality"]))
+
+		metadataOutputVector.extend([metadata["quality"][v] for v in headset.channel_mask])
+		# for i,channel in enumerate(e.channel_mask):
+        #             print "%10s: %.2f %20s: %.2f" % (channel, data[i], "Quality", e.quality[channel])
+		outlets['metadata'].push_sample(pylsl.vectori(metadataOutputVector), pylsl.local_clock())
 		# Send to the UDP server
 		stringMetadata = json.dumps(metadata)
 		clientSock.sendto(stringMetadata, (UDP_IP_ADDRESS, UDP_PORT_NO))
@@ -63,17 +72,30 @@ def dataAcquisitionLoop(headset, outlet, clientSock):
 
 
 def setupLabStreamingLayer(headset):
-	info = pylsl.stream_info('Emotiv EEG', 'EEG', len(headset.channel_mask), headset.sampling_rate, pylsl.cf_float32, str(headset.serial_number))
-	info_desc = info.desc()
-	info_desc.append_child_value("manufacturer", "Emotiv")
-	channels = info_desc.append_child("channels")
+    # (self, name='untitled', type='', channel_count=1, nominal_srate=IRREGULAR_RATE, channel_format=cf_float32, source_id='', handle=None)
+	dataStreamInfo = pylsl.stream_info('Emotiv EEG', 'EEG', len(headset.channel_mask), headset.sampling_rate, pylsl.cf_float32, str(headset.serial_number))
+	dataStreamInfo_desc = dataStreamInfo.desc()
+	dataStreamInfo_desc.append_child_value("manufacturer", "Emotiv")
 
+	# The metadataStream consists of the channel qualities and the battery level
+	metadataStreamInfo = pylsl.stream_info('Emotiv EEG Meta', '', (len(headset.channel_mask) + 1), (1.0 / PacketSendDuration), pylsl.cf_float32, str(headset.serial_number))
+	metadataStreamInfo_desc = metadataStreamInfo.desc()
+	metadataStreamInfo_desc.append_child_value("manufacturer", "Emotiv")
+
+	channels = dataStreamInfo_desc.append_child("channels")
+	channelsMeta = metadataStreamInfo_desc.append_child("channels")
+
+	channelsMeta.append_child("channel").append_child_value("label", "battery").append_child_value("unit","PercentCharge").append_child_value("type","")
 	for ch in headset.channel_mask:
 		channels.append_child("channel").append_child_value("label", ch).append_child_value("unit","microvolts").append_child_value("type","EEG")
+		channelsMeta.append_child("channel").append_child_value("label", ch).append_child_value("unit","EmotivQualityUnits").append_child_value("type","")
 
 	# Outgoing buffer size = 360 seconds, transmission chunk size = 32 samples
-	outlet = pylsl.stream_outlet(info, 1, 32)
-	return (outlet, info_desc)
+	# stream_outlet(info, chunk_size=0, max_buffered=360):
+	# outlets['data'] = pylsl.stream_outlet(dataStreamInfo, 1, 32)
+	# outlets['metadata'] = pylsl.stream_outlet(metadataStreamInfo, 1, 32)
+	# return outlets
+	return {"data": pylsl.stream_outlet(dataStreamInfo, 1, 32), "metadata": pylsl.stream_outlet(metadataStreamInfo, 1, 32)}
 
 def main():
 	print "Running Data Aquisition: Press any key to terminate at the end of the block."
@@ -94,10 +116,10 @@ def main():
 	clientSock.sendto(Message, (UDP_IP_ADDRESS, UDP_PORT_NO))
 
 	# Setup LabStreamingLayer connection
-	(outlet, info_desc) = setupLabStreamingLayer(headset)
+	outlets = setupLabStreamingLayer(headset)
 
 	# Acquire
-	dataAcquisitionLoop(headset, outlet, clientSock)
+	dataAcquisitionLoop(headset, outlets, clientSock)
 	#print "Data Acquisition Terminated."
 	try:
 		headset.disconnect()
